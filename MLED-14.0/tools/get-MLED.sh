0254e7876fe400c4bbf631a4b876ba0966800e31
Kiki Novak <info@microlinux.fr>
2013-06-15 09:14:42 +0200

    ...

---
+++ MLED-14.0/tools/get-MLED.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# Retrieve Microlinux Enterprise Desktop packages 
+
+SERVER="http://www.microlinux.fr"
+DIRECTORY="slackware"
+VERSION="14.0"
+CWD=$(pwd)
+
+case $(uname -m) in 
+  x86_64) TXZDIR=pkg64 ;;
+  *     ) TXZDIR=pkg ;;
+esac  
+
+#/usr/bin/wget \
+#-c \
+#-r \
+#-np \
+#-nH \
+#--cut-dirs=3 \
+#--reject "index.html*" \
+#$SERVER/$DIRECTORY/MLED-$VERSION/$TXZDIR/ -P ../$TXZDIR
+
+echo
+echo ":: All packages downloaded to the $TXZDIR directory. Change into it and"
+echo ":: install everything using the following command:"
+echo "::"
+echo "::   # upgradepkg --reinstall --install-new *.txz"
+echo "::"
+echo 
